d493691882bf8d4f4774228e07445529d66b26e1
Trusha-Bhardava <Trusha2628@gmail.com>
2025-10-07 16:29:21 +0530

    do-while loop in java

---
+++ loops3.java
@@ -0,0 +1,12 @@
+public class loops3 
+{
+      public static void main(String[] args)
+    {
+        int i = 0;
+        do {
+            System.out.print(i + " ");
+            i++;
+        } while (i <= 5);
+    }
+
+}

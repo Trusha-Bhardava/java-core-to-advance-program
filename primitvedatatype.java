45fa9ef1e830440727cb21a6192fce231ce02397
Trusha Bhardava <Trusha2628@gmail.com>
2025-10-07 15:24:17 +0530

    primitve data type program

---
+++ primitvedatatype.java
@@ -0,0 +1,33 @@
+public class primitvedatatype {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+
+         byte a = 25;
+        byte t = -10;
+
+         int p = 2000000;
+        int d = 150000000;
+
+         float pi = 3.14f;
+        float gravity = 9.81f;
+
+         char g = 'A';
+        char s = '$';
+
+        System.out.println("Grade: " + g); 
+        System.out.println("Symbol: " + s); 
+
+        System.out.println("Value of Pi: " + pi);  
+        System.out.println("Gravity: " + gravity); 
+
+        System.out.println("Population: " + p); 
+        System.out.println("Distance: " + d);
+
+        System.out.println("Age: " + a);  
+        System.out.println("Temperature: " + t);
+
+        System.out.println("Is Java fun? " + b1);  
+        System.out.println("Is fish tasty? " + b2); 
+    }
+}

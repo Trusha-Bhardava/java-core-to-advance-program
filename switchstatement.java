d1f89758560db910f9dba75c13153dc83d4e79b4
Trusha-Bhardava <Trusha2628@gmail.com>
2025-10-07 16:18:15 +0530

    switch case using java

---
+++ switchstatement.java
@@ -0,0 +1,24 @@
+public class switchstatement 
+{
+     public static void main(String[] args)
+    {
+        int num = 13;
+        switch (num) {
+        case 5:
+            System.out.println("It is 5");
+            break;
+        case 10:
+            System.out.println("It is 10");
+            break;
+        case 15:
+            System.out.println("It is 15");
+            break;
+        case 20:
+            System.out.println("It is 20");
+            break;
+        default:
+            System.out.println("Not present in case");
+        }
+    }
+    
+}

422c5e42280a02f3458f95e27e3cec0be0e3aba4
Trusha-Bhardava <Trusha2628@gmail.com>
2025-10-07 17:01:15 +0530

    multi-dimensional array

---
+++ array2.java
@@ -0,0 +1,16 @@
+public class array2 {
+
+     public static void main(String args[])
+     {
+         int[][] marks ={{4,7,8}, {5,8,7}, {7,9,14}};
+
+         for (int i=0;i<3;i++)
+         {
+            for (int j=0;j<3;j++)
+            {
+         System.out.print(marks[i][j] + " ");
+         }
+         System.out.println();
+     }
+}
+}

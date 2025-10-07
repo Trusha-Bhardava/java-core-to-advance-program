e0f472bfe53baa0eea9eea8d97c6202ffc2f9bcf
Trusha-Bhardava <Trusha2628@gmail.com>
2025-10-07 16:45:59 +0530

    one dimensional array

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array {
+     public static void main(String args[])
+     {
+         int a[] = {4,7,8};
+
+         for (int i=0;i<a.length;i++)
+         {
+         System.out.println(a[i]);
+         }
+     }
+}

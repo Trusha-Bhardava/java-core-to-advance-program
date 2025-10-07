1d666a0242336aa2e9a9071d4d31a3745540310c
Trusha-Bhardava <Trusha2628@gmail.com>
2025-10-07 15:52:39 +0530

    primitve datatype declare in java

---
+++ variabledeclare.java
@@ -0,0 +1,16 @@
+public class variabledeclare
+{ public static void main(String[] args) 
+    {
+        int age = 25; 
+        float si = 5.5f;
+        String name = "GeeksforGeeks"; 
+        double salary = 50000.50;  
+        char v = 'trusha';
+           
+        System.out.println("Age: " + age); 
+        System.out.println("Simple Interest: " + si);     
+        System.out.println("Name: " + name);        
+        System.out.println("Salary: " + salary);
+        System.out.println("character:" + v);
+    }
+}
